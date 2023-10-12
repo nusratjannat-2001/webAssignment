841fe6432e6d624495d31eb9d93a4aab3287ed2a
Nusrat Jannat <72993483+nusratjannat-2001@users.noreply.github.com>
2023-10-12 13:54:15 +0600

    Update form.php

--- form.php
+++ form.php
@@ -6,22 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-<form method="post" action="createProcess.php">
+<h2 align="center">Add a Book</h2>
+<form align="center" method="post" action="createProcess.php">
     <label for:title>Title : </label>
 <input type="text" name="title" placeholder="Type title"required>
-<br>
+<br><br>
 <label for: Author> Author : </label>
   <input type="text" name="authorName" placeholder="Type author name"required>
-    <br>
+    <br><br>
     <label for: Avaiable> Available : </label>
    <input type="text" name="available" placeholder="Type true/false"required>
-    <br>
+    <br><br>
     <label for: pages> Pages : </label>
    <input type="text" name="pages" placeholder="Type page number"required>
-    <br>
+    <br><br>
     <label for: isbn>Isbn : </label>
    <input type="text" name="isbn" placeholder="Type isbn"required>
-    <br>
+    <br><br>
     <input type="submit" value="Submit">
 </form>
 

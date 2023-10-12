df1eddb849199a0b2a5a6860ce392026bdb46f1d
Nusrat Jannat <72993483+nusratjannat-2001@users.noreply.github.com>
2023-10-12 13:54:52 +0600

    Update edit.php

--- edit.php
+++ edit.php
@@ -15,23 +15,24 @@ $bookToEdit = $bookJson[$index];
     <title>Document</title>
 </head>
 <body>
-<form method="post" action="editProcess.php">
+    <h2 align="center">Edit a Book</h2>
+<form align="center" method="post" action="editProcess.php">
     <input type="hidden" name="indexToEdit" value="<?php echo $index ?>">
     <label for:title>Title : </label>
 <input type="text" name="title" value="<?php echo $bookToEdit['title'] ?>" placeholder="Type title"required>
-<br>
+<br><br>
 <label for: Author> Author : </label>
   <input type="text" name="authorName" value="<?php echo $bookToEdit['author'] ?>" placeholder="Type author name"required>
-    <br>
+    <br><br>
     <label for: Avaiable> Available : </label>
    <input type="text" name="available" value="<?php echo $bookToEdit['available'] ?>" placeholder="Type true/false"required>
-    <br>
+    <br><br>
     <label for: pages> Pages : </label>
    <input type="text" name="pages" value="<?php echo $bookToEdit['pages'] ?>" placeholder="Type page number"required>
-    <br>
+    <br><br>
     <label for: isbn>Isbn : </label>
    <input type="text" name="isbn" value="<?php echo $bookToEdit['isbn'] ?>" placeholder="Type isbn"required>
-    <br>
+    <br><br>
     <input type="submit" value="Submit">
 </form>
 
